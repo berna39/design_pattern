00fd532f439d2d373d1266197240dfd4342c3a0d
berna39 <kalemajoseph39@gmail.com>
2022-01-23 21:42:18 +0200

    First case of factory

--- icloudservice.py
+++ icloudservice.py
@@ -5,3 +5,37 @@ class ICloudService(meta=ABCMeta):
     @abstractstaticmethod
     def auth():
         pass
+
+    @abstractstaticmethod
+    def deploy():
+        pass
+
+    @abstractstaticmethod
+    def scale():
+        pass
+
+class Azure(ICloudService):
+    @abstractstaticmethod
+    def auth():
+        print('Auth to Azure')
+
+    @abstractstaticmethod
+    def deploy():
+        print('Deploy to Azure')
+
+    @abstractstaticmethod
+    def scale():
+        print('Scale my app to Azure')
+
+class CloudServiceFactory:
+     
+    @staticmethod
+    def get_instance(provider):
+        if provider == 'Azure':
+            pass
+        elif provider == 'Aws':
+            pass
+        elif provider == 'Gcp':
+            pass
+        else:
+            return -1 
